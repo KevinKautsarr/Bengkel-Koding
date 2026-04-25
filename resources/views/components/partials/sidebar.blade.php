d72f386fbc6e958fd1afecdfe8b973b40df12835
KevinKautsarr <kevinkautsar6@gmail.com>
2026-04-25 22:08:50 +0700

    Tugas:4

--- resources/views/components/partials/sidebar.blade.php
+++ resources/views/components/partials/sidebar.blade.php
@@ -96,6 +96,12 @@
                 Dashboard Pasien
             </a>
 
+            <a href="{{ route('pasien.daftar') }}"
+                class="{{ $baseLink }} {{ request()->routeIs('pasien.daftar') ? $active : $inactive }}">
+                <i class="fas fa-hospital-user w-4 text-center"></i>
+                Pendaftaran Periksa
+            </a>
+
 
         </div>
         @endif
@@ -114,6 +120,12 @@
                 class="{{ $baseLink }} {{ request()->routeIs('dokter.dashboard') ? $active : $inactive }}">
                 <i class="fas fa-stethoscope w-4 text-center"></i>
                 Dashboard Dokter
+            </a>
+
+            <a href="{{ route('jadwal-periksa.index') }}"
+                class="{{ $baseLink }} {{ request()->routeIs('jadwal-periksa.*') ? $active : $inactive }}">
+                <i class="fas fa-calendar-check w-4 text-center"></i>
+                Jadwal Periksa
             </a>
 
         </div>
